a627042b74beedb4464b794843640102e2e88788
Harsh Srivastava <srivastavaharsh8957@gmail.com>
2024-01-20 21:54:07 +0530

    first commit

--- script.js
+++ script.js
@@ -9,18 +9,36 @@ let arr = [
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+    arr.forEach(element => {
+      if(element.profession === 'developer') {
+        console.log(element);
+      }
+    });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4, name:"susan", age:"20", profession:"intern"});
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i].profession === 'admin') {
+      arr.splice(i,1);
+    }
+  }
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 1, name: 'john', age: 18, profession: 'developer' },
+    { id: 2, name: 'jack', age: 20, profession: 'developer' },
+    { id: 3, name: 'karen', age: 19, profession: 'admin' },
+  ];
+
+  arr.push(...arr2);
 }
 
 // Here is an example of how functions work,
